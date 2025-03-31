614c60946b2aa581e804c9d338372488bc4d3d53
Alan Ng <aaabbbcccddd00001111@gmail.com>
2025-03-31 16:45:23 +0800

    Script

--- Script/Module/MakeModule.cmd
+++ Script/Module/MakeModule.cmd
@@ -5,6 +5,6 @@ set DotNetOutFold=..\Class\Out\net8.0
 
 echo Make Module %Module%
 pushd %DotNetOutFold%
-saber make ../../../View/Module/Text.%Module%
+saber make ../../../Text/Module/Text.%Module%
 echo Status: %errorlevel%
 popd
